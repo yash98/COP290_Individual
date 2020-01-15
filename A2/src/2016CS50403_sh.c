0797ddc94a388663a4c30993fea59495821a212a
yash98 <yashmalviya98@gmail.com>
2020-01-16 04:37:14 +0530

    attached everything basics working

--- A2/src/2016CS50403_sh.c
+++ A2/src/2016CS50403_sh.c
@@ -8,6 +8,7 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <stdlib.h>
+#include <fcntl.h> 
 
 char mainInput[2000];
 
@@ -32,15 +33,18 @@ int parseCmd() {
 
 	fgets(mainInp, sizeof(mainInput), stdin);
 
+	if (*mainInp == '\n') mainloop();
+
 	parallelCommands[cmdIndex].input = strtok(mainInp, "|\n");
 	cmdIndex++;
-	parallelCmdCount = 1;
 
 	while(1) {
 		parallelCommands[cmdIndex].input = strtok(NULL, " \n");
 		if (parallelCommands[cmdIndex].input == NULL) break;
 		parallelCmdCount++;
+		cmdIndex++;
 	}
+	parallelCmdCount = cmdIndex;
 	return 0;
 }
 
@@ -55,30 +59,44 @@ int parseSectionCmd(int commandIndex) {
 	currentCmd->inChanged = 0;
 	currentCmd->outChanged = 0;
 
-	cmdArgs[currentCmd->argCount] = strtok(inp, " ");
-	currentCmd->argCount = 1;
+	if (*inp == ' ') inp++;
+
+	int argCountHere = 0;
+	char* cutPoint = strtok(inp, " ");
+	cmdArgs[argCountHere] = cutPoint;
+	argCountHere++;
 
 	while (1) {
-		cmdArgs[currentCmd->argCount] = strtok(NULL, " ");
-		if (cmdArgs[currentCmd->argCount] == NULL) break;
-		char cmd0 = *cmdArgs[currentCmd->argCount];
-		char cmd1 = *(cmdArgs[currentCmd->argCount]+1);
-		if ((cmd0 == '<') || (cmd0 == '>')) {
+		cutPoint = strtok(NULL, " ");
+		if (cutPoint == NULL) break;
+
+		char cmd0 = *cutPoint;
+		char cmd1 = *(cutPoint+1);
+		if (cmd0 == '<') {
 			if (cmd1 == '\0') {
-				if (cmd0 == '<') {
-					strcpy(inStream, strtok(NULL, " "));
-					currentCmd->inChanged = 1;
-				} else {
-					strcpy(outStream , strtok(NULL, " "));
-					currentCmd->outChanged = 1;
-				}
-				continue;
+				strcpy(inStream, strtok(NULL, " "));
 			} else {
-				cmdArgs[currentCmd->argCount]++;
+				strcpy(inStream, cutPoint+1);
 			}
+			currentCmd->inChanged = 1;
+		} else if (cmd0 == '>') {
+			if (cmd1 == '\0') {
+				strcpy(outStream , strtok(NULL, " "));
+			}
+			else {
+				strcpy(outStream, cutPoint+1);
+			}
+			currentCmd->outChanged = 1;
 		}
-		currentCmd->argCount++;
+		if ((cmd0 == '<') || (cmd0 == '>')) {
+			cutPoint = NULL;
+			argCountHere--;
+		} else {
+			cmdArgs[argCountHere] = cutPoint;
+		}
+		argCountHere++;
 	}
+	currentCmd->argCount = argCountHere;
 	return 0;
 }
 
@@ -133,6 +151,17 @@ int execute(char** argv) {
 int child_status_combined;
 int childsNotReturned;
 
+int pipes[9][2];
+
+int makePipe(int pipeIndex) {
+	int errorCode = pipe(pipes[pipeIndex]);
+	if (errorCode < 0) {
+		printf("System failed to pipe%n");
+		mainloop();
+	}
+	return errorCode;
+}
+
 int execCmd() {
 	child_status_combined = 0;
 	childsNotReturned = parallelCmdCount;
@@ -140,25 +169,89 @@ int execCmd() {
 	for (int i=0; i<parallelCmdCount; i++) {
 		parseSectionCmd(i);
 		char** argv = parallelCommands[i].cmdArgsArr;
-		
+
+		// // sample code
+		// // open input and output files
+		// int in = open("scores", O_RDONLY);
+		// int out = open("out", O_WRONLY | O_TRUNC | O_CREAT, S_IRUSR | S_IRGRP | S_IWGRP | S_IWUSR);
+
+		// // replace standard input with input file
+		// dup2(in, 0);
+
+		// // replace standard output with output file
+		// dup2(out, 1);
+
+		// // close unused file descriptors
+		// close(in);
+		// close(out);
+
+		// connnect pipes
+		if (parallelCmdCount > 1) {
+			if (i == 0) {
+				makePipe(i);
+				dup2(pipes[i][1], 1);
+				close(pipes[i][1]);
+			} else if (i == parallelCmdCount-1) {
+				dup2(pipes[i-1][0], 0);
+				close(pipes[i-1][0]);
+			} else {
+				makePipe(i);
+				dup2(pipes[i][1], 1);
+				close(pipes[i][1]);
+				dup2(pipes[i-1][0], 0);
+				close(pipes[i-1][0]);
+			}
+		}
+
+		int errorCode = 0;
 		if (strcmp(argv[0], "exit") == 0) exit(0);
-		
-		int ret_pid = fork();
-		if(ret_pid == 0) {
-			/* This is done by the child process. */
-			if (strcmp(argv[0], "cd") == 0) exit(cd(argv[1]));
-			else if (strcmp(argv[0], "pwd") == 0) exit(pwd());
-			else if (strcmp(argv[0], "mkdir") == 0) exit(mkdirec(argv[1]));
-			else if (strcmp(argv[0], "rmdir") == 0) exit(rmdirec(argv[1]));
-			else exit(execute(argv));
+		else if (strcmp(argv[0], "cd") == 0) {
+			errorCode = cd(argv[1]) ; 
+			childsNotReturned--;
+		}
+		else if (strcmp(argv[0], "pwd") == 0) {
+			errorCode = pwd(); 
+			childsNotReturned--;
+		}
+		else if (strcmp(argv[0], "mkdir") == 0) {
+			errorCode = mkdirec(argv[1]); 
+			childsNotReturned--;
+		}
+		else if (strcmp(argv[0], "rmdir") == 0) {
+			errorCode = rmdirec(argv[1]); 
+			childsNotReturned--;
+		} else {
+			int ret_pid = fork();
+			if(ret_pid == 0) {
+				/* This is done by the child process. */
+
+				// connect input output
+				if (parallelCommands[i].inChanged) {
+					int in = open(parallelCommands[i].inStreamBuff, O_RDONLY);
+					dup2(in, 0);
+					close(in);
+				}
+
+				if (parallelCommands[i].outChanged) {
+					int out = open(parallelCommands[i].outStreamBuff, O_WRONLY | O_TRUNC | O_CREAT, S_IRUSR | S_IRGRP | S_IWGRP | S_IWUSR);
+					dup2(out, 1);
+					close(out);
+				}
+				
+				printf("%d: %s\n", ret_pid, argv[0]);
+				exit(execute(argv));
+			}
 		}	
+
+		child_status_combined = child_status_combined || errorCode;
 	}
 	/* This is run by the parent.  Wait for the child
 	to terminate. */
 	while (childsNotReturned>0) {
 		int childStatus;
 		pid_t childId = wait(&childStatus);
-		if (childStatus > 0) child_status_combined = 1;
+		printf("%d\n", childId);
+		child_status_combined = child_status_combined || 1;
 
 		if (childId == -1) return -1;
 		else childsNotReturned--;
@@ -166,19 +259,16 @@ int execCmd() {
 	return 0;
 }
 
-int main() {
+int mainloop() {
 	while(1) {
 		printf("shell> ");
 		parseCmd();
 		execCmd();
 	}
+	return 0;
 }
 
-// This shell cant do multiple redirections or complex pipes like
-// 	cat < a.txt < b.txt
-// or 
-// 	cmd1 | cmd2 | cmd 
-// or even 
-// 	cmd1 < a.txt | cmd2 > b.txt
-// but it can do 
-// 	cmd1 < a.txt > b.txt
+int main() {
+	mainloop();
+	return 0;
+}
