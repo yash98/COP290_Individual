778b69c507ca1adb2271a310656984dd650b1df3
Yash Malviya <yashmalviya98@gmail.com>
2020-02-28 02:04:43 +0530

    maybe correct

--- A5/src/decomment.c
+++ A5/src/decomment.c
@@ -7,7 +7,7 @@ extern char* yytext;
 
 int main(void) 
 {
-	int token, dqo, sqo, co;
+	int token, dqo = 0, sqo = 0, co = 0;
 	
 	token = yylex();
 
@@ -17,31 +17,34 @@ int main(void)
 				if (!(dqo || sqo)) {
 					co = 1;
 					fprintf(stdout, " ");
-				} else fprintf(stdout, "%s\n", yytext);
+				} else fprintf(stdout, "%s", yytext);
 				break;
 			case comment_end:
 				if (!(dqo || sqo || !co)) co = 0;
-				else fprintf(stdout, "%s\n", yytext);
+				else fprintf(stdout, "%s", yytext);
 				break;
 			case double_qoute:
 				if (!(co || sqo)) dqo = !(dqo);
-				fprintf(stdout, "%s\n", yytext);
+				fprintf(stdout, "%s", yytext);
 				break;
 			case single_qoute:
 				if (!(co || dqo)) sqo = !(sqo);
-				fprintf(stdout, "%s\n", yytext);
+				fprintf(stdout, "%s", yytext);
 				break;
 			case nextline:
-				fprintf(stdout, "%s\n", yytext);
+				fprintf(stdout, "%s", yytext);
 				break;
 			case rest:
-				if (!co) fprintf(stdout, "%s\n", yytext);
+				if (!co) {
+					fprintf(stdout, "%s", yytext);
+				}
 				break;
 			default:
 				fprintf(stderr, "unknown string\n");
 				return exit_failure;
 		}
-	
+
+		// fflush(stdout);
 		token = yylex();
 	}
 
