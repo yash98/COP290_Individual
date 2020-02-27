c09190c165c4a75bfe8750a710f5a91c177cffe1
Yash Malviya <yashmalviya98@gmail.com>
2020-02-28 00:11:48 +0530

    first draft

--- A5/src/decomment.c
+++ A5/src/decomment.c
@@ -14,13 +14,26 @@ int main(void)
 	while(token) {
 		switch (token) {
 			case comment_start:
-				if (!(dqo || sqo)) co = 1;
+				if (!(dqo || sqo)) {
+					co = 1;
+					fprintf(stdout, " ");
+				} else fprintf(stdout, yytext);
 			case comment_end:
 				if (!(dqo || sqo)) co = 0;
+				else fprintf(stdout, yytext);
 			case double_qoute:
 				if (!(co || sqo)) dqo = !(dqo);
+				fprintf(stdout, yytext);
 			case single_qoute:
 				if (!(co || dqo)) sqo = !(sqo);
+				fprintf(stdout, yytext);
+			case nextline:
+				fprintf(stdout, yytext);
+			case rest:
+				if (!co) fprintf(stdout, yytext);
+			default:
+				fprintf(stderr, "unknown string\n");
+				return exit_failure;
 		}
 	
 		token = yylex();
